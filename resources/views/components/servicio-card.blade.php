72900821d72f1d28c7dd0b9e294f52a7a2f85c05
CarranzaIvan <113496779+CarranzaIvan@users.noreply.github.com>
2024-10-25 13:17:27 -0600

    Agregar al carrito.
    Se ha contabilizado la cantidad de articulos a contar.

--- resources/views/components/servicio-card.blade.php
+++ resources/views/components/servicio-card.blade.php
@@ -1,10 +1,10 @@
 <!-- Tarjeta de mostrar producto -->
 <div class="col-md-4 mb-4">
-    <div class="card h-100 shadow-sm d-flex flex-column"> <!-- Usar flex para el contenedor de la tarjeta -->
-        <div class="card-body text-start d-flex flex-column flex-grow-1"> <!-- Usar flex en el cuerpo de la tarjeta -->
+    <div class="card h-100 shadow-sm d-flex flex-column"> 
+        <div class="card-body text-start d-flex flex-column flex-grow-1"> 
             <h5 class="card-title font-weight-bold servicio-titulo">{{ $titulo }}</h5>
             <h6 class="servicio-precio text-secondary"> $ {{ $precio }}</h6>
-            <div class="text-center mb-3"> <!-- Agregar un margen inferior -->
+            <div class="text-center mb-3"> 
                 <img src="{{ asset($imagen ? 'storage/' . $imagen : 'images/default.png') }}" 
                      class="img-fluid img-servicio" 
                      alt="{{ $titulo }}" 
@@ -12,9 +12,10 @@
                      style="width: 100%; height: 200px; object-fit: cover;">
             </div>
             <p class="font-weight-normal servicio-descripcion flex-grow-1">{{ $descripcion }}</p>
-            <div class="mt-auto"> <!-- Asegurarse de que el botón esté al final -->
-                <button class="btn btn-agregar" data-id="{{ $idServicio }}">
-                    <i class="bi bi-cart"></i> Agregar al carrito
+            <div class="mt-auto"> 
+                <button class="btn btn-agregar" data-id="{{ $idServicio }}"
+                        onclick="toggleCarrito('{{ $idServicio }}', '{{ $titulo }}', '{{ $descripcion }}', this)">
+                    Agregar al carrito
                 </button>
             </div>
         </div>
